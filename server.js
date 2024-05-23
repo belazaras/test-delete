a691b5e58f983d40347cb802f293d2e6a2cb9faf
Nicolas Belazaras <belazaras@live.com>
2024-05-23 12:17:28 +0100

    test.

---
+++ server.js
@@ -0,0 +1,17 @@
+import { createRequestHandler } from "@remix-run/express";
+import express from "express";
+
+// notice that the result of `remix vite:build` is "just a module"
+import * as build from "./build/server/index.js";
+import 'dotenv/config'
+
+const app = express();
+app.use(express.static("build/client"));
+
+// and your app is "just a request handler"
+app.all("*", createRequestHandler({ build }));
+
+console.log(process.env.GREETING);
+app.listen(3000, () => {
+  console.log("App listening on http://localhost:3000");
+});
